c871ffb789b9c1de43688839fb5b209c3edae210
Cab2da <cab2da@naver.com>
2021-09-07 15:26:06 +0900

    Firebase 서버에 배포하기

--- src/components/Words.js
+++ src/components/Words.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
-import { withStyles } from '@material-ui/core/Styles';
+import { withStyles } from '@material-ui/core/styles';
 import Grid from  '@material-ui/core/Grid';
 import Button from  '@material-ui/core/Button';
 import Fab from  '@material-ui/core/Fab';
@@ -71,9 +71,9 @@ class Words extends React.Component {
             this.setState({words: nextState});
         });
     }
-    shouldComponentUpdate(nextProps, nextState) {
-        return nextState != this.state.words;
-    }
+    // shouldComponentUpdate(nextProps, nextState) {
+    //     return nextState != this.state.words;
+    // }
     componentDidMount() {
         this._get();
     }
@@ -129,20 +129,20 @@ class Words extends React.Component {
                 })}
                 <Fab color="primary" className={classes.fab} onClick={this.handleDialogToggle}>
                     <AddIcon />
-                    <Dialog open={this.state.dialog} onClose={this.handleDialogToggle}>
-                        <DialogTitle>단어 추가</DialogTitle>
-                        <DialogContent>
-                            <TextField label="단어" type="text" name="word" value={this.state.word} onChange={this.handleValueChange} /> <br />
-                            <TextField label="가중치" type="text" name="weight" value={this.state.weight} onChange={this.handleValueChange} /> <br />
-                        </DialogContent>
-                        <DialogActions>
-                            <Button variant="contained" color="primary" onClick={this.handleSubmit}>추가</Button>
-                            <Button variant="outlined" color="primary" onClick={this.handleDialogToggle}>닫기</Button>
-                        </DialogActions>
-                    </Dialog>
                 </Fab>
+                <Dialog open={this.state.dialog} onClose={this.handleDialogToggle}>
+                    <DialogTitle>단어 추가</DialogTitle>
+                    <DialogContent>
+                        <TextField label="단어" type="text" name="word" value={this.state.word} onChange={this.handleValueChange} /> <br />
+                        <TextField label="가중치" type="text" name="weight" value={this.state.weight} onChange={this.handleValueChange} /> <br />
+                    </DialogContent>
+                    <DialogActions>
+                        <Button variant="contained" color="primary" onClick={this.handleSubmit}>추가</Button>
+                        <Button variant="outlined" color="primary" onClick={this.handleDialogToggle}>닫기</Button>
+                    </DialogActions>
+                </Dialog>
             </div>
-        )
+        );
     }
 }
 
